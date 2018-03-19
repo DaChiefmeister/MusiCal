7bdc438ef54102a60fc530015fd96662649f858d
Nate Wilkens <nate_wilkens@hotmail.com>
2018-03-18 23:37:51 -0400

    js stuff

---
+++ faqExpander.js
@@ -0,0 +1,21 @@
+function toggle() {
+    var h2 = this;
+    if(h2.getAttribute('class') == 'plus'){
+        h2.setAttribute('class', 'minus');
+    }
+    else{
+        h2.setAttribute('class', 'plus');
+    }
+    if(h2.nextElementSibling.getAttribute('class') == 'closed'){
+        h2.nextElementSibling.setAttribute('class', 'open');
+    }
+    else{
+        h2.nextElementSibling.setAttribute('class', 'closed');
+    }
+}
+window.onload = function(){
+    var h2Elements = document.getElementById('faqs').getElementsByTagName('h2');
+            for(i=0; i<h2Elements.length; i++){
+                h2Elements[i].onclick = toggle;
+            }
+        }
